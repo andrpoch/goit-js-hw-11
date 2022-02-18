bf2597b44965bfdd4b13e02435e0240c0d0e2f1e
andrpoch <andrpoch25@gmail.com>
2022-02-18 19:31:10 +0200

    In progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,100 @@
 import './sass/main.scss';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchImages } from './services/axiosImages';
+
+const form = document.querySelector('#search-form');
+const input = document.querySelector('input[name="searchQuery"]');
+const gallery = document.querySelector('.gallery');
+const btn = document.querySelector('.load-more');
+
+let page = 1;
+let inputValue = '';
+
+const getImages = async (e) => {
+   e.preventDefault();
+   inputValue = input.value.trim();
+
+   if (!inputValue) {
+      hideLoadBtn();
+      clearData();
+      return;
+   }
+
+   page = 1;
+   hideLoadBtn();
+   clearData();
+
+   try {
+      const res = await fetchImages(inputValue, page); 
+        if (res.hits.length === 0) {
+      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      return;
+        }
+      renderImg(res.hits);
+       Notify.success(`Hooray! We found ${res.totalHits} images.`);
+       if (res.hits.length < 40) {
+      return;
+    }
+      loadBtn();
+   } catch (error) {
+    Notify.failure('Sorry, something went wrong. Please try again.');
+   }
+}
+
+const renderImg = (arr) => {
+   const markup = arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, dowloads }) => {
+      return `<div class="photo-card">
+ <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300"/>
+</a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${dowloads}
+    </p>
+  </div>
+</div>`
+   })
+      .join('');
+   gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+const loadImages = async () => {
+   page += 1;
+   btn.disabled = true;
+
+  try {
+     const res = await fetchImages(inputValue, page); 
+         renderImg(res.hits);
+       if (res.hits.length < 40) {
+          hideLoadMoreBtn();
+        Notify.info("We're sorry, but you've reached the end of search results.");
+      return;
+    }
+      loadBtn();
+   } catch (error) {
+   Notify.failure('Sorry, there are no images matching your search query. Please try again.'); 
+   }
+}
+
+const clearData = () => {
+   gallery.innerHTML = '';
+}
+const loadBtn = ()=> {
+  btn.classList.remove('is-hidden');
+  btn.disabled = false;
+}
+const hideLoadBtn = () => {
+  btn.disabled = true;
+   btn.classList.add('is-hidden');
+}
+
+form.addEventListener('submit', getImages);
+btn.addEventListener('click', loadImages);
--- src/services/axiosImages.js
+++ src/services/axiosImages.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-const API_KEY = '25752976-c432b2b3d55e7cabed38c70ac';
+const API_KEY = 'key=25752976-c432b2b3d55e7cabed38c70ac';
 const PARAMETERS = 'image_type=photo&orientation=horizontal&safesearch=true';
 const PER_PAGE = 'per_page=40';
-let PAGE = 1;
 
-const getImages = (value) => {
-   return axios.get(`?${API_KEY}&q=${value}&${PARAMETERS}&page=${PAGE}&${PER_PAGE}`).then(res => res.data);
+const fetchImages = async (value,page) => {
+   const res = await axios.get(`?${API_KEY}&q=${value}&${PARAMETERS}&page=${page}&${PER_PAGE}`);
+   return res.data;
 }
+
+export { fetchImages };
